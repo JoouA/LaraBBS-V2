285fd2100ae33809e83f6a1edaed66837406f889
JoouA <tangwtna@163.com>
2017-12-24 23:59:46 +0800

    User profile page

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['show']);
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user)
+    {
+
+    }
+}
