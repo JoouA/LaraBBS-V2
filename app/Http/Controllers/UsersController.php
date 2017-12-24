7a1463f86778c338f8e9d2e957d015f23ddfcbbd
JoouA <tangwtna@163.com>
2017-12-25 01:23:17 +0800

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,23 +7,44 @@ use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
+    /**
+     * UsersController constructor.
+     */
     public function __construct()
     {
         $this->middleware('auth')->except(['show']);
     }
 
+    /**
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(User $user)
     {
         return view('users.show',compact('user'));
     }
 
+    /**
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function edit(User $user)
     {
+        $this->authorize('edit',$user);
         return view('users.edit',compact('user'));
     }
 
+    /**
+     * @param UserRequest $request
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function update(UserRequest $request,User $user)
     {
+        $this->authorize('update',$user);
+
         $user->update($request->all());
 
         return redirect()->route('users.show',$user->id)->with('success','更新个人资料成功!');
