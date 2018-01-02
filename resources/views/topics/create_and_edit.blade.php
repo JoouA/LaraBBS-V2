e09ba88ac336b482aa20b2466149533a83c1c24e
JoouA <tangwtna@163.com>
2018-01-02 22:32:27 +0800

    Prevent xss injection

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -43,7 +43,7 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
+                                    <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少三个字符的内容。" required> {!! old('body', $topic->body )  !!}</textarea>
                                 </div>
 
                                 <div class="well well-sm">
