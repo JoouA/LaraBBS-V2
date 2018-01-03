abee3ac6993043a9dd8e7dc5016d044ba3513bfc
JoouA <tangwtna@163.com>
2018-01-03 23:02:32 +0800

    Add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,6 +2,7 @@
 namespace  App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -9,6 +10,12 @@ use App\Models\Topic;
 
 class TopicObserver
 {
+
+    /*
+     * 当一个新模型被初次保存将会触发 creating 以及 created 事件。
+     * 如果一个模型已经存在于数据库且调用了 save 方法，将会触发 updating 和 updated 事件。
+     * 在这两种情况下都会触发 saving 和 saved 事件。
+     * */
     /**
      * @param Topic $topic
      */
@@ -20,9 +27,19 @@ class TopicObserver
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
 
+    }
+
+    /**
+     * @param Topic $topic
+     */
+    public function saved(Topic $topic)
+    {
         // 如果没有slug字段没有内容，使用翻译
+        // 当文章第一次被创建的时候，在saving过程中还没有id，所以在在saving中调用队列会报错
         if (! $topic->slug){
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            dispatch(new TranslateSlug($topic));
+            //$topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
         }
     }
+
 }
