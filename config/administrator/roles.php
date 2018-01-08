f3922dd39e495a207fdcffceb2f54d9431b2f2d2
JoouA <tangwtna@163.com>
2018-01-08 22:57:19 +0800

    Admin can manger roles and permissions

---
+++ config/administrator/roles.php
@@ -0,0 +1,71 @@
+<?php
+use Spatie\Permission\Models\Role;
+
+return [
+    'title' => '角色',
+    'single' => '角色',
+    'model' => Role::class,
+
+    'permission' => function(){
+        return Auth::user()->can('manage_users');
+    },
+    'columns' => [
+        'id' => [
+            'title' => 'ID',
+        ],
+        'name' => [
+            'title' => '标志',
+        ],
+        'permissions' => [
+            'title' => '权限',
+            'output' => function($value,$model){
+                $model->load('permissions');
+                $result = [];
+                foreach ($model->permissions as $permission){
+                    $result[] = $permission->name;
+                }
+                return empty($result)? 'N/A' : implode($result,'|');
+            },
+            'sortable' => false,
+        ],
+        'operation' => [
+            'title' => '管理',
+            'output' => function($value,$model){
+                return $value;
+            },
+            'sortable' => false,
+        ],
+    ],
+
+    'edit_fields' => [
+        'name' => [
+            'title' => '标志',
+        ],
+        'permissions' => [
+            'title' => '权限',
+            'type' => 'relationship',
+            'name_field' => 'name',
+        ],
+    ],
+
+    'filters' => [
+      'id' => [
+          'title' => 'ID',
+      ],
+      'name' => [
+        'title' => '标志',
+      ],
+    ],
+
+    // 新建和编辑时的表单验证规则
+    'rules' => [
+        'name' => 'required|max:15|unique:roles,name',
+    ],
+
+    // 表单验证错误时定制错误消息
+    'messages' => [
+        'name.required' => '标志不能为空',
+        'name.max' => '标志的长度最大为15',
+        'name.unique' => '标志已存在',
+    ],
+];
