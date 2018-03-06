d410ff1efd94f0199ed07e425889b70283348622
JoouA <tangwtna@163.com>
2018-03-06 10:17:46 +0800

    generate-token command

---
+++ app/Console/Commands/GenerateToken.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class GenerateToken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'larabbs:generate-token';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '快速为用户生成token';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $userId = $this->ask('亲输入用户ID');
+
+         $user = User::find($userId);
+         if (!$user){
+             return $this->error('用户不存在');
+         }
+
+         // 一年后过期
+        $ttl = 365*24*60;
+        $this->info(\Auth::guard('api')->setTTL($ttl)->fromUser($user));
+    }
+}
