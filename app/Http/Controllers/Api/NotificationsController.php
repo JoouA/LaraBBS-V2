3ffb1fcf7158dde5d2af36278508d419f74140fb
JoouA <tangwtna@163.com>
2018-03-09 16:17:24 +0800

    notifications stats

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -7,10 +7,21 @@ use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
+    /**
+     * @return \Dingo\Api\Http\Response
+     */
     public function index()
     {
-        $notifications = $this->user->notifications()->paginate(20);
+        $notifications = $this->user()->notifications()->paginate(20);
 
         return $this->response->paginator($notifications,new NotificationTransformer());
+    }
+
+    public  function stats()
+    {
+        // 是user表中的数据
+        return $this->response->array([
+            'unread_count' => $this->user()->notification_count,
+        ]);
     }
 }
