f9d3d5f86995129790da0dd3e810f374bad8cc87
JoouA <tangwtna@163.com>
2018-03-10 15:30:00 +0800

    locale

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,5 +60,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'founder' => \App\Http\Middleware\FounderPermission::class,
+        'change-locale' =>  \App\Http\Middleware\ChangeLocale::class,
     ];
 }
